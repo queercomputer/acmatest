8005beec237c576337e4bbfbc29e545cd2c18456
queercomputer <103514724+queercomputer@users.noreply.github.com>
2024-06-13 15:49:03 +1000

    javascript

--- index.js
+++ index.js
@@ -72,30 +72,6 @@ if (!isInstalledPWA && installNudge && !hideBanner) {
 
 /************************************************************************
 
-Feature: Menu tabbing
-
-*************************************************************************/
-
-document.addEventListener('DOMContentLoaded', function() {
-  console.log("Script loaded");
-  
-  const hamburger = document.getElementById('hamburger');
-  const mobileLinks = document.getElementById('mobile-links');
-
-  if (hamburger && mobileLinks) {
-      hamburger.addEventListener('click', function(event) {
-          event.preventDefault();
-          console.log("Hamburger clicked");
-          mobileLinks.classList.toggle('open');
-          hamburger.classList.toggle('open');
-      });
-  } else {
-      console.error("Hamburger or mobile links element not found");
-  }
-});
-
-/************************************************************************
-
 Feature: Notifications
 
 *************************************************************************/
