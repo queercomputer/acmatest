7960ea065be4d3aeeb553000cf231eb17a90002e
queercomputer <103514724+queercomputer@users.noreply.github.com>
2024-06-13 15:31:02 +1000

    Update index.js

--- index.js
+++ index.js
@@ -283,12 +283,19 @@ screen.orientation.addEventListener("change", function (e) {
 showOrientationBlocks();
 
 document.addEventListener('DOMContentLoaded', function() {
+  console.log("Script loaded");
+  
   const hamburger = document.getElementById('hamburger');
   const mobileLinks = document.getElementById('mobile-links');
 
-  hamburger.addEventListener('click', function(event) {
-      event.preventDefault();
-      mobileLinks.classList.toggle('open');
-      hamburger.classList.toggle('open');
-  });
+  if (hamburger && mobileLinks) {
+      hamburger.addEventListener('click', function(event) {
+          event.preventDefault();
+          console.log("Hamburger clicked");
+          mobileLinks.classList.toggle('open');
+          hamburger.classList.toggle('open');
+      });
+  } else {
+      console.error("Hamburger or mobile links element not found");
+  }
 });
