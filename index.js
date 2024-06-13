dd825e7c2d8d960474b2add2036f052f161ceada
queercomputer <103514724+queercomputer@users.noreply.github.com>
2024-06-13 15:41:08 +1000

    Javascript

--- index.js
+++ index.js
@@ -282,20 +282,3 @@ screen.orientation.addEventListener("change", function (e) {
 // show/hide orientation classes
 showOrientationBlocks();
 
-document.addEventListener('DOMContentLoaded', function() {
-  console.log("Script loaded");
-  
-  const hamburger = document.getElementById('hamburger');
-  const mobileLinks = document.getElementById('mobile-links');
-
-  if (hamburger && mobileLinks) {
-      hamburger.addEventListener('click', function(event) {
-          event.preventDefault();
-          console.log("Hamburger clicked");
-          mobileLinks.classList.toggle('open');
-          hamburger.classList.toggle('open');
-      });
-  } else {
-      console.error("Hamburger or mobile links element not found");
-  }
-});
